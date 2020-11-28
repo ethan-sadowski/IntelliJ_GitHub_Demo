18309c04aee5bc7011511245d9cb6459cf6c8341
Ethan <esadowski1@my.bcit.ca>
2020-11-27 18:07:09 -0800

    Initial commit

---
+++ src/IntelliJ_Demo.java
@@ -0,0 +1,7 @@
+public class IntelliJ_Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+        System.out.println("Please store my code!");
+    }
+}
